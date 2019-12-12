98cdea77a003f725098507709e06038fa5cd1de2
mohamed idbrahim <idbrahimdev@gmail.com>
2019-12-12 16:05:00 +0100

    cros origin

---
+++ config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+   
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['http://localhost:4200'],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
